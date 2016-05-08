76e0fb80fb3117ccddb216ed4714c0e8354503fc
Cian Byrne <Cian Byrne>
2016-05-09 01:12:30 +1000

    fixed mingw compile issues, added flags into compile.sh file.

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-gcc -Wall -Werror -std=gnu11 -pthread main.c matrix.c -o matrix -lm
+gcc -Wall -Werror -DHAVE_STRUCT_TIMESPEC -D_GNU_SOURCE=1 -std=gnu11 -pthread main.c matrix.c -o matrix -lm
 #-fsanitize=address
